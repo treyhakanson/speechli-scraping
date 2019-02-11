1665f0486fde5a59da9d8ae937eb082d77cd4ac6
lpatel16 <31355800+lpatel16@users.noreply.github.com>
2019-02-11 15:11:20 -0500

    Updated genius crawler
    Updated genius crawler to attempt to retrieve song title and lyrics

--- src/crawler/genius.py
+++ src/crawler/genius.py
@@ -1,4 +1,5 @@
 """Genius sub-crawler."""
+import re
 import csv
 from .constants import DATA_DIR
 from .crawlable import Crawlable
@@ -62,24 +63,83 @@ def clean(link):
 
 
 def write(fpath, data):
-    """Write data to a csv."""
-    with fpath.open(mode="w") as csvfile:
-        csvwriter = csv.writer(
-            csvfile,
-            delimiter=',',
-            quoting=csv.QUOTE_MINIMAL
-        )
-        csvwriter.writerows(data)
+    """Write data to a csv, or dump it in a text file."""
+    if isinstance(data, list):
+        with fpath.open(mode="w") as csvfile:
+            csvwriter = csv.writer(
+                csvfile,
+                delimiter=",",
+                quoting=csv.QUOTE_MINIMAL
+            )
+            csvwriter.writerows(data)
+    else:
+        with fpath.open(mode="w") as f:
+            f.write(data)
 
 
-def retrieve_index():
+def get_title(index_url):
+    """Get the title of the song based on the index link."""
+
+    crawlable = Crawlable(index_url)
+    crawlable.retrieve()
+    if crawlable.success:
+        
+        # Problem 1: Not properly parsing the title of the song
+        data = [text for text in crawlable.parse("h1 > .header_with_cover_art-primary_info-title")]
+        print(data)
+    return data
+
+
+def retrieve_index(no_cache=False, **kwargs):
     """Retrieve the genius indexes."""
     setup()
     for loc, url in BASE_URLS:
         fpath = BASE_DIR.joinpath(loc)
+        print(f"{url} -> {loc}...")
+        if not no_cache and fpath.exists():
+            # If we are using the cache and the result exists, skip
+            print(f"\tUsing cached results.")
+            continue
         if not fpath.parent.exists():
             fpath.parent.mkdir(parents=True)
         crawlable = Crawlable(url)
         crawlable.retrieve()
-        data = [(clean(link),) for link in crawlable.parse("div > .mini_card")]
-        write(fpath, data)
+        if crawlable.success:
+            data = [(clean(link),) for link in crawlable.parse("div > .mini_card")]
+            write(fpath, data)
+            print(f"\tSuccessfully retrieved index.")
+        else:
+            print(f"\tNo content available.")
+
+
+def retrieve_from_index(no_cache=False, **kwargs):
+    """Retrieve the lyrics from the index."""
+    for loc, url in BASE_URLS:
+        fpath = BASE_DIR.joinpath(loc)
+        with fpath.open(mode="r") as csvfile:
+            csvreader = csv.reader(
+                csvfile,
+                delimiter=",",
+                quoting=csv.QUOTE_MINIMAL
+            )
+            for row in csvreader:
+                index_url = row[0]
+                title = get_title(index_url)
+                song_fpath = fpath.parent.joinpath(f"{title}.txt")
+                if not no_cache and song_fpath.exists():
+                    # If we are using the cache and the result exists, skip
+                    print(f"\tUsing cached results.")
+                    continue
+                crawlable = Crawlable(index_url)
+                crawlable.retrieve()
+
+                # Problem 2: Site layout makes retrieving lyrics complex (certain parts of lyrics embedded in different html elements)
+                text = crawlable.parse("div > .lyrics")
+                write(song_fpath, text)
+                print(f"\tSuccessfully retrieved lyrics.")
+
+
+def fetch(**kwargs):
+    """Fetch the indexes, and the retrieve the lyrics from those indexes."""
+    retrieve_index(**kwargs)
+    retrieve_from_index(**kwargs)
---
+++ src/parser/genius.py
@@ -0,0 +1,61 @@
+"""Parser for content from Genius."""
+import re
+import json
+from .constants import DATA_DIR
+
+BASE_DIR = DATA_DIR.joinpath("genius")
+
+
+def iterate(fpath):
+    """Iterate over arbitrarily nested directories."""
+    if not fpath.is_dir():
+        yield fpath
+    else:
+        for subpath in fpath.iterdir():
+            yield from iterate(subpath)
+
+
+def parse(no_cache=False, **kwargs):
+    """Parse all content from Genius."""
+    content_start_re = re.compile(r"^(?:\*{3}[^*]*\*{3})$", re.M)
+    regexes = [
+        ("title", re.compile(r"Title:\s+(.*)")),
+        ("author", re.compile(r"Author:\s+(.*)")),
+        (
+            "releaseDate",
+            re.compile(r"^Release\s+Date:\s+([\w\s,]+)(?:\s\[.*\])?$", re.M)
+        ),
+        ("language", re.compile(r"Language:\s+(.*)"))
+    ]
+    for fpath in iterate(BASE_DIR):
+        if "".join(fpath.suffixes) != ".txt":
+            continue
+        output_fpath = fpath.parent.joinpath(f"parsed/{fpath.name[:-4]}.json")
+        print(f"{fpath} -> {output_fpath}")
+        if not no_cache and output_fpath.exists():
+            print("Using cache.")
+            continue
+        data = {}
+        preamble = ""
+        f = fpath.open(mode="r")
+        line = f.readline()
+        while not content_start_re.match(line):
+            preamble += f"{line}\n"
+            line = f.readline()
+            if line == "":
+                raise Exception("Malformed file")
+        for key, regex in regexes:
+            try:
+                data[key] = regex.search(preamble).group(1)
+            except AttributeError as e:
+                raise Exception(
+                    f"Failed to match \"{key}\" regex in:\n",
+                    preamble
+                )
+        data["content"] = f.read()
+        f.close()
+        if not output_fpath.parent.exists():
+            output_fpath.parent.mkdir(parents=True)
+        with output_fpath.open(mode="w") as f:
+            json.dump(data, f)
+        print("Done.")
